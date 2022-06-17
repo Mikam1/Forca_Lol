78d4716d749a70ae6a7405bdf22f5db77f1d3490
Samuel Ferracini <samferracini@gmail.com>
2022-06-16 21:04:23 -0300

    cria menu inicial

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("@@ Hello world @@")
+"""
+TODO
+
+1 - Menu inicial
+2 - Escolher campeão filtrando pelo input do usuário
+
+"""
+
+import json
+  
+championsNames = json.load(open('champions.json'))
+
+def main():
+    print('')
+    print('     FORCA LOL')
+    print('     1 - Iniciar') # FUNÇÃO INCIAR JOGO
+    print('     2 - Sair')    # SAIR DO PROGRAMA
+    print('')
+
+main()
